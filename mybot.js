eb703f4c18d5ccd626b66431c68c7cb55a323eff
nathancharles <nathancookster@gmail.com>
2014-07-23 11:42:57 -0500

    Store the fruit locations.

--- mybot.js
+++ mybot.js
@@ -1,28 +1,41 @@
 function new_game() {
 }
 
+var fruitLocations;
+
 function make_move() {
-   var board = get_board();
+	var board = get_board();
 
-   // we found an item! take it!
-   if (board[get_my_x()][get_my_y()] > 0) {
-       return TAKE;
-   }
+	if(!fruitLocations) {
+		fruitLocations = [];
+		board.map(function(column, xCoor) {
+			column.map(function(cell, yCoor) {
+				if(cell > 0) {
+					fruitLocations.push([xCoor, yCoor]);
+				}
+			});
+		});
+	}
 
-   var rand = Math.random() * 4;
+	// we found an item! take it!
+	if (board[get_my_x()][get_my_y()] > 0) {
+		return TAKE;
+	}
 
-   if (rand < 1) return NORTH;
-   if (rand < 2) return SOUTH;
-   if (rand < 3) return EAST;
-   if (rand < 4) return WEST;
+	var rand = Math.random() * 4;
 
-   return PASS;
+	if (rand < 1) return NORTH;
+	if (rand < 2) return SOUTH;
+	if (rand < 3) return EAST;
+	if (rand < 4) return WEST;
+
+	return PASS;
 }
 
-// Optionally include this function if you'd like to always reset to a 
+// Optionally include this function if you'd like to always reset to a
 // certain board number/layout. This is useful for repeatedly testing your
 // bot(s) against known positions.
 //
 //function default_board_number() {
-//    return 123;
+//	return 123;
 //}
