d7f62aa1ee72fc266f20b62039d3148a6500ba3b
nathancharles <nathancookster@gmail.com>
2014-07-24 09:03:21 -0500

    Add logic to match AI.

--- mybot.js
+++ mybot.js
@@ -1,35 +1,70 @@
-function new_game() {
+
+var fruits;
+
+function _updateLocations() {
+	var board = get_board();
+	fruits = fruits.filter(function(fruit, index) {
+		return board[fruit.x][fruit.y] > 0;
+	});
 }
 
-var fruitLocations;
+function _getNearestLocation(myX, myY) {
+	var arr = fruits.map(function(fruit, index) {
+		return Math.abs(myX - fruit.x) + Math.abs(myY - fruit.y);
+	});
+	var index = arr.indexOf(Math.min.apply(Math, arr));
+	return fruits[index];
+}
+
+function new_game() {
+	fruits = [];
+	get_board().map(function(column, xCoor) {
+		column.map(function(cell, yCoor) {
+			if(cell > 0) {
+				fruits.push({x: xCoor, y: yCoor, type: cell});
+			}
+		});
+	});
+}
 
 function make_move() {
+	_updateLocations();
 	var board = get_board();
-
-	if(!fruitLocations) {
-		fruitLocations = [];
-		board.map(function(column, xCoor) {
-			column.map(function(cell, yCoor) {
-				if(cell > 0) {
-					fruitLocations.push([xCoor, yCoor]);
-				}
-			});
-		});
-	}
+	var myX = get_my_x();
+	var myY = get_my_y();
+	var retVal;
 
 	// we found an item! take it!
-	if (board[get_my_x()][get_my_y()] > 0) {
+	if (board[myX][myY] > 0) {
 		return TAKE;
 	}
 
-	var rand = Math.random() * 4;
+	var target = _getNearestLocation(myX, myY);
 
-	if (rand < 1) return NORTH;
-	if (rand < 2) return SOUTH;
-	if (rand < 3) return EAST;
-	if (rand < 4) return WEST;
+	var xDiff = target.x - myX;
+	var yDiff = target.y - myY;
 
-	return PASS;
+	// console.log(myX, myY);
+	// console.log(target.x, target.y);
+	// console.log(xDiff, yDiff);
+
+	if(!!yDiff) {
+		retVal = (yDiff > 0) ? SOUTH : NORTH;
+	}
+
+	if(!!xDiff) {
+		retVal = (xDiff > 0) ? EAST : WEST;
+	}
+
+	return retVal;
+	// var rand = Math.random() * 4;
+
+	// if (rand < 1) return NORTH;
+	// if (rand < 2) return SOUTH;
+	// if (rand < 3) return EAST;
+	// if (rand < 4) return WEST;
+
+	// return PASS;
 }
 
 // Optionally include this function if you'd like to always reset to a
